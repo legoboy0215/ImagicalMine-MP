23ff12dd1db84fdb9759e237422837aa39ad45c3
caspervanneck <wesleyvanneck1012@gmail.com>
2017-04-07 10:28:49 +0200

    Add files via upload

--- src/pocketmine/event/EventPriority.php
+++ src/pocketmine/event/EventPriority.php
@@ -1,36 +1,26 @@
 <?php
-/**
- * src/pocketmine/event/EventPriority.php
- *
- * @package default
- */
-
 
 /*
  *
- *  _                       _           _ __  __ _
- * (_)                     (_)         | |  \/  (_)
- *  _ _ __ ___   __ _  __ _ _  ___ __ _| | \  / |_ _ __   ___
- * | | '_ ` _ \ / _` |/ _` | |/ __/ _` | | |\/| | | '_ \ / _ \
- * | | | | | | | (_| | (_| | | (_| (_| | | |  | | | | | |  __/
- * |_|_| |_| |_|\__,_|\__, |_|\___\__,_|_|_|  |_|_|_| |_|\___|
- *                     __/ |
- *                    |___/
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
- * This program is a third party build by ImagicalMine.
- *
- * PocketMine is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author ImagicalMine Team
- * @link http://forums.imagicalmine.net/
+ * @author PocketMine Team
+ * @link   http://www.pocketmine.net/
  *
  *
-*/
+ */
 
 namespace pocketmine\event;
+
 
 /**
  * List of event priorities
@@ -40,40 +30,34 @@ namespace pocketmine\event;
  *
  * MONITOR events should not change the event outcome or contents
  */
-abstract class EventPriority
-{
+abstract class EventPriority{
+	/**
+	 * Event call is of very low importance and should be ran first, to allow
+	 * other plugins to further customise the outcome
+	 */
+	const LOWEST = 5;
+	/**
+	 * Event call is of low importance
+	 */
+	const LOW = 4;
+	/**
+	 * Event call is neither important or unimportant, and may be ran normally
+	 */
+	const NORMAL = 3;
+	/**
+	 * Event call is of high importance
+	 */
+	const HIGH = 2;
+	/**
+	 * Event call is critical and must have the final say in what happens
+	 * to the event
+	 */
+	const HIGHEST = 1;
+	/**
+	 * Event is listened to purely for monitoring the outcome of an event.
+	 *
+	 * No modifications to the event should be made under this priority
+	 */
+	const MONITOR = 0;
 
-    /**
-     * Event call is of very low importance and should be ran first, to allow
-     * other plugins to further customise the outcome
-     */
-    const LOWEST = 5;
-
-    /**
-     * Event call is of low importance
-     */
-    const LOW = 4;
-
-    /**
-     * Event call is neither important or unimportant, and may be ran normally
-     */
-    const NORMAL = 3;
-
-    /**
-     * Event call is of high importance
-     */
-    const HIGH = 2;
-
-    /**
-     * Event call is critical and must have the final say in what happens
-     * to the event
-     */
-    const HIGHEST = 1;
-
-    /**
-     * Event is listened to purely for monitoring the outcome of an event.
-     *
-     * No modifications to the event should be made under this priority
-     */
-    const MONITOR = 0;
 }
--- src/pocketmine/event/TranslationContainer.php
+++ src/pocketmine/event/TranslationContainer.php
@@ -1,103 +1,78 @@
 <?php
-/**
- * src/pocketmine/event/TranslationContainer.php
- *
- * @package default
- */
-
 
 /*
  *
- *  _                       _           _ __  __ _
- * (_)                     (_)         | |  \/  (_)
- *  _ _ __ ___   __ _  __ _ _  ___ __ _| | \  / |_ _ __   ___
- * | | '_ ` _ \ / _` |/ _` | |/ __/ _` | | |\/| | | '_ \ / _ \
- * | | | | | | | (_| | (_| | | (_| (_| | | |  | | | | | |  __/
- * |_|_| |_| |_|\__,_|\__, |_|\___\__,_|_|_|  |_|_|_| |_|\___|
- *                     __/ |
- *                    |___/
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
- * This program is a third party build by ImagicalMine.
- *
- * PocketMine is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author ImagicalMine Team
- * @link http://forums.imagicalcorp.ml/
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
  *
  *
 */
 
 namespace pocketmine\event;
 
-class TranslationContainer extends TextContainer
-{
+class TranslationContainer extends TextContainer{
 
-    /** @var string[] $params */
-    protected $params = [];
+	/** @var string[] $params */
+	protected $params = [];
 
-    /**
-     *
-     * @param string   $text
-     * @param string[] $params
-     */
-    public function __construct($text, array $params = [])
-    {
-        parent::__construct($text);
+	/**
+	 * @param string   $text
+	 * @param string[] $params
+	 */
+	public function __construct($text, array $params = []){
+		parent::__construct($text);
 
-        $this->setParameters($params);
-    }
+		$this->setParameters($params);
+	}
 
+	/**
+	 * @return string[]
+	 */
+	public function getParameters(){
+		return $this->params;
+	}
 
-    /**
-     *
-     * @return string[]
-     */
-    public function getParameters()
-    {
-        return $this->params;
-    }
+	/**
+	 * @param int $i
+	 *
+	 * @return string
+	 */
+	public function getParameter($i){
+		return isset($this->params[$i]) ? $this->params[$i] : null;
+	}
 
+	/**
+	 * @param int    $i
+	 * @param string $str
+	 */
+	public function setParameter($i, $str){
+		if($i < 0 or $i > count($this->params)){ //Intended, allow to set the last
+			throw new \InvalidArgumentException("Invalid index $i, have " . count($this->params));
+		}
 
-    /**
-     *
-     * @param int     $i
-     * @return string
-     */
-    public function getParameter($i)
-    {
-        return isset($this->params[$i]) ? $this->params[$i] : null;
-    }
+		$this->params[(int) $i] = $str;
+	}
 
+	/**
+	 * @param string[] $params
+	 */
+	public function setParameters(array $params){
+		$i = 0;
+		foreach($params as $str){
+			$this->params[$i] = (string) $str;
 
-    /**
-     *
-     * @param int     $i
-     * @param string  $str
-     */
-    public function setParameter($i, $str)
-    {
-        if ($i < 0 or $i > count($this->params)) { //Intended, allow to set the last
-            throw new \InvalidArgumentException("Invalid index $i, have " . count($this->params));
-        }
-
-        $this->params[(int) $i] = $str;
-    }
-
-
-    /**
-     *
-     * @param string[] $params
-     */
-    public function setParameters(array $params)
-    {
-        $i = 0;
-        foreach ($params as $str) {
-            $this->params[$i] = (string) $str;
-
-            ++$i;
-        }
-    }
+			++$i;
+		}
+	}
 }
