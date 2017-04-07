124e4d6d2ff877e6d58a76d7549184eb861551e6
caspervanneck <wesleyvanneck1012@gmail.com>
2017-04-07 13:37:58 +0200

    Update SetEntityDataPacket.php

--- src/pocketmine/network/protocol/SetEntityDataPacket.php
+++ src/pocketmine/network/protocol/SetEntityDataPacket.php
@@ -2,19 +2,24 @@
 
 /*
  *
- *  ____            _        _   __  __ _                  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
- * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *  _                       _           _ __  __ _
+ * (_)                     (_)         | |  \/  (_)
+ *  _ _ __ ___   __ _  __ _ _  ___ __ _| | \  / |_ _ __   ___
+ * | | '_ ` _ \ / _` |/ _` | |/ __/ _` | | |\/| | | '_ \ / _ \
+ * | | | | | | | (_| | (_| | | (_| (_| | | |  | | | | | |  __/
+ * |_|_| |_| |_|\__,_|\__, |_|\___\__,_|_|_|  |_|_|_| |_|\___|
+ *                     __/ |
+ *                    |___/
  *
- * This program is free software: you can redistribute it and/or modify
+ * This program is a third party build by ImagicalMine.
+ *
+ * ImagicalMine is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author PocketMine Team
- * @link http://www.pocketmine.net/
+ * @author ImagicalMine Team
+ * @link http://forums.imagicalcorp.ml/
  *
  *
 */
@@ -23,6 +28,10 @@ namespace pocketmine\network\protocol;
 
 #include <rules/DataPacket.h>
 
+#ifndef COMPILE
+use pocketmine\utils\Binary;
+
+#endif
 
 class SetEntityDataPacket extends DataPacket{
 	const NETWORK_ID = Info::SET_ENTITY_DATA_PACKET;
@@ -37,7 +46,8 @@ class SetEntityDataPacket extends DataPacket{
 	public function encode(){
 		$this->reset();
 		$this->putEntityId($this->eid);
-		$this->putEntityMetadata($this->metadata);
+		$meta = Binary::writeMetadata($this->metadata);
+		$this->put($meta);
 	}
 
 }
