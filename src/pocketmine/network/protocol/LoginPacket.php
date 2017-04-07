924247db3f1547324b98b835e49d3fce51eadc32
caspervanneck <wesleyvanneck1012@gmail.com>
2017-04-07 17:45:31 +0200

    Update LoginPacket.php

--- src/pocketmine/network/protocol/LoginPacket.php
+++ src/pocketmine/network/protocol/LoginPacket.php
@@ -2,19 +2,24 @@
 
 /*
  *
- *  ____            _        _   __  __ _                  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
- * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *  _                       _           _ __  __ _
+ * (_)                     (_)         | |  \/  (_)
+ *  _ _ __ ___   __ _  __ _ _  ___ __ _| | \  / |_ _ __   ___
+ * | | '_ ` _ \ / _` |/ _` | |/ __/ _` | | |\/| | | '_ \ / _ \
+ * | | | | | | | (_| | (_| | | (_| (_| | | |  | | | | | |  __/
+ * |_|_| |_| |_|\__,_|\__, |_|\___\__,_|_|_|  |_|_|_| |_|\___|
+ *                     __/ |
+ *                    |___/
  *
- * This program is free software: you can redistribute it and/or modify
+ * This program is a third party build by ImagicalMine.
+ *
+ * PocketMine is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author PocketMine Team
- * @link http://www.pocketmine.net/
+ * @author ImagicalMine Team
+ * @link http://forums.imagicalcorp.ml/
  *
  *
 */
@@ -24,85 +29,42 @@ namespace pocketmine\network\protocol;
 #include <rules/DataPacket.h>
 
 
-class LoginPacket extends DataPacket{
-	const NETWORK_ID = Info::LOGIN_PACKET;
+class LoginPacket extends DataPacket
+{
+    const NETWORK_ID = Info::LOGIN_PACKET;
 
-	const EDITION_POCKET = 0;
+    public $username;
+    public $protocol1;
+    public $protocol2;
+    public $clientId;
 
-	public $username;
-	public $protocol;
-	public $gameEdition;
-	public $clientUUID;
-	public $clientId;
-	public $identityPublicKey;
-	public $serverAddress;
+    public $clientUUID;
+    public $serverAddress;
+    public $clientSecret;
 
-	public $skinId;
-	public $skin = null;
+    public $skinId;
+    public $transparency;
+    public $skin = null;
 
-	public $clientData = [];
+    public function decode()
+    {
+        $this->username = $this->getString();
+        $this->protocol1 = $this->getInt();
+        $this->protocol2 = $this->getInt();
+        $this->clientId = $this->getLong();
+        $this->clientUUID = $this->getUUID();
+        $this->serverAddress = $this->getString();
+        $this->clientSecret = $this->getString();
 
-	public $deviceModel;
-	public $os;
+        $this->skinId = $this->getString();
+        $this->transparency = false; //$this->getByte() > 0;
+        $this->skin = $this->getString();
+    }
 
-	public function decode(){
-		$this->protocol = $this->getInt();
+    public function encode()
+    {
+    }
+}
 
-		if($this->protocol !== Info::CURRENT_PROTOCOL){
-			$this->buffer = null;
-			return; //Do not attempt to decode for non-accepted protocols
-		}
 
-		$this->gameEdition = $this->getByte();
-
-		$str = zlib_decode($this->getString(), 1024 * 1024 * 64);
-
-		$this->setBuffer($str, 0);
-
-		$chainData = json_decode($this->get($this->getLInt()));
-		foreach($chainData->{"chain"} as $chain){
-			$webtoken = $this->decodeToken($chain);
-			if(isset($webtoken["extraData"])){
-				if(isset($webtoken["extraData"]["displayName"])){
-					$this->username = $webtoken["extraData"]["displayName"];
-				}
-				if(isset($webtoken["extraData"]["identity"])){
-					$this->clientUUID = $webtoken["extraData"]["identity"];
-				}
-				if(isset($webtoken["identityPublicKey"])){
-					$this->identityPublicKey = $webtoken["identityPublicKey"];
-				}
-			}
-		}
-
-		$this->clientData = $this->decodeToken($this->get($this->getLInt()));
-
-		$this->clientId = $this->clientData["ClientRandomId"] ?? null;
-		$this->serverAddress = $this->clientData["ServerAddress"] ?? null;
-		$this->skinId = $this->clientData["SkinId"] ?? null;
-
-		if(isset($this->clientData["SkinData"])){
-			$this->skin = base64_decode($this->clientData["SkinData"]);
-		}
-
-		if(isset($this->clientData["DeviceModel"])){
-			$this->deviceModel = $this->clientData["DeviceModel"];
-		}
-
-		if(isset($this->clientData["DeviceOS"])){
-			$this->os = $this->clientData["DeviceOS"];
-		}
-
-	}
-
-	public function encode(){
-
-	}
-
-	public function decodeToken($token){
-		$tokens = explode(".", $token);
-		list($headB64, $payloadB64, $sigB64) = $tokens;
-
-		return json_decode(base64_decode($payloadB64), true);
-	}
 }
